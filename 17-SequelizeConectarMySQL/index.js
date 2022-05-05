16bb8b8bff29328b40c70960797a4e29b53ba16f
Gabriel Salvador <102549645+ogabrielsalvador@users.noreply.github.com>
2022-05-05 16:10:26 -0300

    att

--- 17-SequelizeConectarMySQL/index.js
+++ 17-SequelizeConectarMySQL/index.js
@@ -1,6 +1,26 @@
 // COMO SE CONECTAR AO BANCO DE DADOS ATRAVÉS DO SEQUELIZE
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('')
+const sequelize = new Sequelize('sistemadecadastro', 'root', 'senha', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+/*
+const sequelize = new Sequelize('NomeDoBancoDeDadosQueVcQuerAcessar', 'Usuario', 'Senha', {
+    host: 'QualHost',
+    dialect: 'QualBancoDeDados'
+})
+*/
 
 
+// FORMA DE CONFERIR SE FOI FEITA A CONEXÃO
+sequelize.authenticate().then(function(){
+    console.log('Conectado com sucesso!')
+}).catch(function(erro){
+    console.log('Falha ao se conectar: ' + erro)
+})
+
+// o ".authenticate()" irá conferir se foi possível conectar, resultando em dois casos:
+// o uso do ".then()" no caso de conseguir se conectar
+// ou o uso do ".catch()" no caso de não conseguir se conectar
